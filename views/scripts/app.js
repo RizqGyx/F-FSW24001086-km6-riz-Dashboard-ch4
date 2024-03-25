0772870b546e1fce5ffe22d7897affb93ac5f2ea
RizqGyx <mhdrizky32@gmail.com>
2024-03-25 11:19:08 +0700

    Adding: Project

---
+++ views/scripts/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const carRoute = require("../../routes/carRoutes");
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use("/cars", carRoute);
+
+module.exports = app;
